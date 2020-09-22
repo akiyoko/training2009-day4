3d92879e030a9624d974d95936abba103ecd1c47
akiyoko <akiyoko@users.noreply.github.com>
2020-09-23 00:17:25 +0900

    修正

--- todo/views.py
+++ todo/views.py
@@ -55,8 +55,8 @@ class TodoListView(View):
             'today': today,
             'todo_list': todo_list,
         }
-        # return TemplateResponse(request, 'todo/todo_list.html', context)
-        return TemplateResponse(request, 'todo/todo_list_1_3.html', context)
+        return TemplateResponse(request, 'todo/todo_list.html', context)
+        # return TemplateResponse(request, 'todo/todo_list_1_3.html', context)
 
 
 # Step 6. TODO追加画面を作成する
